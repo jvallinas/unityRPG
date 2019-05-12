14de3a11a0bbf974f378c27940bd3a1b26afec67
Javier <javier.vallinas.lima@gmail.com>
2019-05-12 20:34:32 +0200

    Match Animation To Movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,9 +10,11 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -20,8 +22,17 @@ public class Mover : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit) {
+        if (hasHit)
+        {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
