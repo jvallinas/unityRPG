68412dccc3bb1e31f9a085edfd9bb20c0a77f210
Javier <javier.vallinas.lima@gmail.com>
2019-05-13 23:12:56 +0200

    Taking Damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,6 +9,7 @@ namespace RPG.Combat
 
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
@@ -33,6 +34,7 @@ namespace RPG.Combat
         private void AttackBehaviour()
         {
             if (timeSinceLastAttack > timeBetweenAttacks) {
+                // This will trigger the Hit() event
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
             }
@@ -56,7 +58,8 @@ namespace RPG.Combat
         // Animation Event
         void Hit()
         {
-
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
     }
 
