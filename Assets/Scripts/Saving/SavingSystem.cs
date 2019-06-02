31571c21927326dc81b04aed08a68d36fb33c4d6
Javier <ff_valli@hotmail.com>
2019-06-02 15:03:38 +0200

    Converting To Binary In C#

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -12,10 +13,17 @@ namespace RPG.Saving
             print("Saving to " + path);
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
-                stream.Write(bytes, 0, bytes.Length);
+                Transform playerTransform = getPlayerTransform();
+                byte[] buffer = SerializeVector(playerTransform.position);
+                stream.Write(buffer, 0, buffer.Length);
             }
         }
+
+        private Transform getPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
         public void Load(string saveFile)
         {
             string path = GetPathFromSaveFile(saveFile);
@@ -25,9 +33,29 @@ namespace RPG.Saving
             {
                 byte[] buffer = new byte[stream.Length];
                 stream.Read(buffer, 0, buffer.Length);
-                string result = Encoding.UTF8.GetString(buffer);
-                print("Loaded info. " + result);
+
+                Transform playerTransform = getPlayerTransform();
+                playerTransform.position = DeserializeVector(buffer);
             }
+        }
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+
+        }
+
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
         }
 
         private string GetPathFromSaveFile(string saveFile)
