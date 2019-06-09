4eebefbe80f90bdbcfd19448aea4ceb8d6b13837
Javier <ff_valli@hotmail.com>
2019-06-09 11:17:55 +0200

    Fading Before Scene Load

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -9,9 +9,14 @@ namespace RPG.SceneManagement
     {
 
         CanvasGroup canvasGroup;
-        private void Start()
+        private void Awake()
         {
             canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public void FadeOutImmediate()
+        {
+            canvasGroup.alpha = 1;
         }
 
         public IEnumerator FadeOut(float time)
--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
 using System.Collections;
+using RPG.Saving;
 
-namespace RPG.Saving
+namespace RPG.SceneManagement
 {
-    
-    public class SavingWrapper : MonoBehaviour 
+
+    public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
 
-        private IEnumerator Start() {
+        [SerializeField] float fadeInTime = 0.2f;
+        private IEnumerator Start()
+        {
+
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
             yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
         }
 
-        private void Update() {
+        private void Update()
+        {
             if (Input.GetKeyDown(KeyCode.S))
             {
                 Save();
@@ -33,5 +41,5 @@ namespace RPG.Saving
             GetComponent<SavingSystem>().Save(defaultSaveFile);
         }
     }
-    
+
 }
