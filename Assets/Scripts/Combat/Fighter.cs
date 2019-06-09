50eb9a7aeb187b5f344108df85f5db36e92ec6a5
Javier <ff_valli@hotmail.com>
2019-06-09 15:35:58 +0200

    Equip A Weapon

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,9 +10,16 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
+        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Transform handTransform = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
+
+        private void Start()
+        {
+            SpawnWeapon();
+        }
 
         private void Update()
         {
@@ -30,6 +37,12 @@ namespace RPG.Combat
                 AttackBehaviour();
             }
         }
+
+        private void SpawnWeapon()
+        {
+            Instantiate(weaponPrefab, handTransform);
+        }
+
 
         private void AttackBehaviour()
         {
